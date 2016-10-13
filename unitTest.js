f895aeb10ac9cb46b09cc60dc957ce7bf558a767
rrd <rrd@webmania.cc>
2016-10-13 12:18:48 +0200

    update for qunit 2.x

--- unitTest.js
+++ unitTest.js
@@ -23,79 +23,79 @@ var sample = {
 		}
 	};
 
-test('$.sessionStorage Test', function(){
-	deepEqual($.sessionStorage('thisShouldBeNull'),
+QUnit.test('$.sessionStorage Test', function(assert){
+	assert.deepEqual($.sessionStorage('thisShouldBeNull'),
 				 null,
 				 'getter: non existent key returned null');
 	
-	deepEqual($.sessionStorage('testNamespace', {van : true}),
+	assert.deepEqual($.sessionStorage('testNamespace', {van : true}),
 				 {van : true},
 				 'setter: returned the correct object');
 	
-	deepEqual($.sessionStorage('testNamespace', {nincs: 5}),
+	assert.deepEqual($.sessionStorage('testNamespace', {nincs: 5}),
 				 {van : true, nincs : 5},
 				 'setter: added new property and returned the new full object');
 	
-	deepEqual($.sessionStorage('testNamespace', {van : false, nincs: 5}),
+	assert.deepEqual($.sessionStorage('testNamespace', {van : false, nincs: 5}),
 				 {van : false, nincs : 5},
 				 'setter: overwrote a property and returned the new full object when given unchanged property');
 	
-	deepEqual($.sessionStorage('testNamespace', {van : 13}),
+	assert.deepEqual($.sessionStorage('testNamespace', {van : 13}),
 				 {van : 13, nincs : 5},
 				 'setter: overwrote a property and returned the new full object when given only what changed');
 
-	deepEqual($.sessionStorage.remove('testNamespace.nincs'),
+	assert.deepEqual($.sessionStorage.remove('testNamespace.nincs'),
 				 {van : 13},
 				 'remove: removed a sub-object and return the new object');
 
 
-	deepEqual($.sessionStorage('testNamespace'),
+	assert.deepEqual($.sessionStorage('testNamespace'),
 				 {van : 13},
 				 'getter: gave the updated object after deleting one property');
 
-	deepEqual($.sessionStorage.remove('testNamespace'),
+	assert.deepEqual($.sessionStorage.remove('testNamespace'),
 				 null,
 				 'remove: returned null after deleting the whole object');
 	
-	deepEqual($.sessionStorage('testNamespace', {a: 125}),
+	assert.deepEqual($.sessionStorage('testNamespace', {a: 125}),
 				 {a: 125},		//TODO this should not be a object, just simply true
 				 'setter: returned the correct object');
 
 	$.sessionStorage('sample', sample);
 	
-	deepEqual($.sessionStorage('sample.widget.debug'),
+	assert.deepEqual($.sessionStorage('sample.widget.debug'),
 				 "on",
 				 'getter: returned the proper sub-sub-object when called by dot notation');
 	
 	$.sessionStorage.remove('sample.widget.window');
 	$.sessionStorage.remove('sample.widget.image');
-	deepEqual($.sessionStorage.remove('sample.widget.text'),
+	assert.deepEqual($.sessionStorage.remove('sample.widget.text'),
 				 {widget : {debug : 'on'}},
 				 'remove: removed a sub-object and return the new object');    
 });
 
-test('$.localStorage Test', function(){
-	deepEqual($.localStorage('testNamespace'),
+QUnit.test('$.localStorage Test', function(assert){
+	assert.deepEqual($.localStorage('testNamespace'),
 				 null,
 				 'getter: returned null, as this namespace does not exists in $.localStorage, only in $.sessionStorage');
 	
-	deepEqual($.localStorage('testNamespace', {object: {level1 : {level2 : {level3 : 'level3'}}}}),
+	assert.deepEqual($.localStorage('testNamespace', {object: {level1 : {level2 : {level3 : 'level3'}}}}),
 				 {object: {level1 : {level2 : {level3 : 'level3'}}}},
 				 'setter: returned the new multi-level object');
 	
-	deepEqual($.localStorage('testNamespace.object'),
+	assert.deepEqual($.localStorage('testNamespace.object'),
 				 {level1 : {level2 : {level3 : 'level3'}}},
 				 'getter: returned the proper sub-object when called by dot notation');
 	
-	deepEqual($.localStorage('testNamespace.object.level1.level2'),
+	assert.deepEqual($.localStorage('testNamespace.object.level1.level2'),
 				 {level3 : 'level3'},
 				 'getter: returned the proper sub-sub-object when called by dot notation');
 	
-	deepEqual($.localStorage.remove('testNamespace'),
+	assert.deepEqual($.localStorage.remove('testNamespace'),
 				 null,
 				 'remove: removed the object and returned null');
 	
-	deepEqual($.localStorage('testNamespace'),
+	assert.deepEqual($.localStorage('testNamespace'),
 				 null,
 				 'setter: returned null after the previous test deleted our object');
 });
