c126828ffda74f7fc08e9463d56549ee4daa0cd6
rrd <rrd@webmania.cc>
2016-11-12 23:14:16 +0100

    quotes changed

--- jquery.rStorage.js
+++ jquery.rStorage.js
@@ -16,14 +16,14 @@
 *
 */
 JSON.unflatten = function(data) {
-    "use strict";
+    'use strict';
     if (Object(data) !== data || Array.isArray(data))
         return data;
     var result = {}, cur, prop, idx, last, temp;
     for(var p in data) {
-        cur = result, prop = "", last = 0;
+        cur = result, prop = '', last = 0;
         do {
-            idx = p.indexOf(".", last);
+            idx = p.indexOf('.', last);
             temp = p.substring(last, idx !== -1 ? idx : undefined);
             cur = cur[prop] || (cur[prop] = (!isNaN(parseInt(temp)) ? [] : {}));
             prop = temp;
@@ -31,7 +31,7 @@ JSON.unflatten = function(data) {
         } while(idx >= 0);
         cur[prop] = data[p];
     }
-    return result[""];
+    return result[''];
 };
 JSON.flatten = function(data) {
     var result = {};
@@ -40,20 +40,20 @@ JSON.flatten = function(data) {
             result[prop] = cur;
         } else if (Array.isArray(cur)) {
             for(var i=0, l=cur.length; i<l; i++)
-                recurse(cur[i], prop ? prop+"."+i : ""+i);
+                recurse(cur[i], prop ? prop+'.'+i : ''+i);
             if (l == 0)
                 result[prop] = [];
         } else {
             var isEmpty = true;
             for (var p in cur) {
                 isEmpty = false;
-                recurse(cur[p], prop ? prop+"."+p : p);
+                recurse(cur[p], prop ? prop+'.'+p : p);
             }
             if (isEmpty)
                 result[prop] = {};
         }
     }
-    recurse(data, "");
+    recurse(data, '');
     return result;
 };
 
